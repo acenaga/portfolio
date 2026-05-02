43609de0334a4002333bab076663a7c42c46549d
Prof. Acenaga <carlos.ferrer@davinci.edu.ar>
2026-05-02 16:43:30 -0300

    adding mockup in view

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::view('portfolio', 'portfolio')->name('portfolio');
+
 require __DIR__.'/settings.php';
